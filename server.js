f608947abf24390fabaf1a07a0b6faabb7b8bec4
JPlatts <japlatts@gmail.com>
2022-03-17 20:02:21 -0400

    SPA route debugging

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.use('/api/deciders', require('./routes/deciders'));
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development') {
     app.use(express.static('client/dist'))
-    app.get('*', (req, res) => {
+    app.get('*/*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'))
     })
 }
