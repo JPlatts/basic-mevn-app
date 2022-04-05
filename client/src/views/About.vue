35cc47016dc2b79a4dfae900ef55f2de87b3458d
JPlatts <japlatts@gmail.com>
2022-04-05 18:42:17 -0400

    about page updates

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,8 +1,31 @@
 <template>
-  <div>
-    <h1>ABOUT</h1>
-    <h3>Version 1.0.0</h3>
-  
+  <div class="container px-4 py-5">
+    <div class="row">
+      <div class="col">
+        <h2 class="pb-2 border-bottom">About</h2>
+        <p> This is a basic single page web application (SPA) with an API for jwt based authentication, account registration, and password management.</p>
+        <p>This project is a great starter for building a web application with membership on the mevn stack.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <p>The GitHub repository for this website is publicly available <a href="https://github.com/JPlatts/basic-mevn-app">here</a>.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-3">
+        <img src="@/assets/MongoDB_Logo_RGB_Logo_Forest-Green.svg" width="100" />
+      </div>
+      <div class="col-sm-3">
+        <img src="@/assets/express-js-seeklogo.com.svg" width="100"/>
+      </div>
+      <div class="col-sm-3">
+        <img src="@/assets/Node.js_logo.svg" width="100"/>
+      </div>
+      <div class="col-sm-3">
+        <img src="@/assets/vue-js-seeklogo.com.svg" width="100" />
+      </div>
+    </div>
   </div>
 </template>
 <script>
